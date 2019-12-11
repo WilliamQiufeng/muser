0fa739e31690b4909d8aefd5a85c93ec7533751a
Qiufeng54321 <williamcraft@163.com>
2019-12-11 12:54:14 +0800

    Update install script

--- __init__.py
+++ __init__.py
@@ -2,5 +2,7 @@ import sprites
 import sheet
 import game
 import util
+import game_config
+import assets
 
-__all__ = ["sprites", "sheet", "game", "util"]
+__all__ = ["sprites", "sheet", "game", "util", "game_config", "assets"]
