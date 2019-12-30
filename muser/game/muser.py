6da35ef9b44404072dd89aef1db38560b4dee849
Qiufeng54321 <williamcraft@163.com>
2019-12-30 18:35:04 +0800

    debugging...

--- muser/game/muser.py
+++ muser/game/muser.py
@@ -8,21 +8,21 @@ import time
 
 class Muser:
     def __init__(self):
-        # pyxel.run(self.update, self.draw)
-        self.quit = False
-        while not self.quit:
-            self.update()
-            self.draw()
-            pyxel.flip()
-            time.sleep(0.01)
+        pyxel.run(self.update, self.draw)
+        # self.quit = False
+        # while not self.quit:
+        #     self.update()
+        #     self.draw()
+        #     pyxel.flip()
+        #     time.sleep(0.01)
     def update(self):
         try: 
             if Config.CAST.is_finished():
                 Config.CAST = Config.CAST.next_cast()
             Config.CAST.update()
-            if pyxel.btn(pyxel.KEY_E):
-                self.quit = True
-                print("Quit")
+            # if pyxel.btn(pyxel.KEY_E):
+            #     self.quit = True
+            #     print("Quit")
         except BaseException as e:
             print(e)
             traceback.print_exc()
