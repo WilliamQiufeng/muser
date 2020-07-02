2dfe9bc9aa98ee9488cb7c8207c0211f832b9cf3
Qiufeng54321 <williamcraft@163.com>
2020-07-02 18:57:37 +0800

    deprecated generator.py

--- muser/sheet/gen/generator.py
+++ muser/sheet/gen/generator.py
@@ -1,8 +1,10 @@
 from .rel_input import SourceSheetInput
 import logger
+import warnings
 
 
 def gen_init():
+    warnings.warn("Relative sheets are no longer supported", DeprecationWarning)
     process = SourceSheetInput(
         "/williamye/program/pyxel_projects/muser/test/test.data")
     process.process_all()
