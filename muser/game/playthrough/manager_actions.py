32736da59b38c81d915c9f5eaea678245e4b3473
Qiufeng54321 <williamcraft@163.com>
2020-07-02 08:16:36 +0800

    Using getattr for manager_actions, reduced redundancy

--- muser/game/playthrough/manager_actions.py
+++ muser/game/playthrough/manager_actions.py
@@ -27,26 +27,27 @@
 '''
 
 
-from sheet.gen.abs_output import *
+import sheet.gen.abs_output as note_types
 from game.playthrough.note import PositionedNote
-from game.playthrough.effect.frame_effect import *
-from game.playthrough.effect.move_effect import *
-from game.playthrough.effect.criteria_effect import *
-from game.playthrough.fancy_note import *
+# They are not unused, they are accessed through globals()
+# flake8: noqa: F401
+from game.playthrough.effect.frame_effect import FrameEffect
+from game.playthrough.effect.move_effect import MoveEffect
+from game.playthrough.effect.criteria_effect import CriteriaEffect
+from game.playthrough.effect.fancy_effect import FancyEffect
+from game.playthrough.fancy_note import StartEffectNote, EndEffectNote
+
+
 class ManagerActions:
     @staticmethod
     def from_note(note):
-        if isinstance(note, AbsNote):
+        note_type: str = type(note).__name__
+        if isinstance(note, note_types.AbsNote):
             return PositionedNote(note)
-        elif isinstance(note, StartFancy):
-            return StartEffectNote(FancyEffect, note)
-        elif isinstance(note, EndEffect):
+        elif isinstance(note, note_types.EndEffect):
             return EndEffectNote(note)
-        elif isinstance(note, StartFrame):
-            return StartEffectNote(FrameEffect, note)
-        elif isinstance(note, StartMove):
-            return StartEffectNote(MoveEffect, note)
-        elif isinstance(note, StartCriteria):
-            return StartEffectNote(CriteriaEffect, note)
+        elif hasattr(note_types, note_type):
+            # note_type[5:] : Remove 'Start' at type name.
+            return StartEffectNote(globals()[note_type[5:] + "Effect"], note)
         else:
             return note
